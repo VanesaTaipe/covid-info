8991736b686c945328741b1a9cdc99f02fa112d2
Angel Zuñiga <angelzuniga@MacBook-Pro-de-Angel.local>
2022-11-21 20:02:31 -0500

    Agregando ignorados

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
-from datetime import date
 """['id_persona', 'fecha_fallecimiento', 'edad', 'sexo',
        'criterio_fallecido', 'ubigeo_cdc', 'dpt_cdc', 'prov_cdc', 'dist_cdc',
        'cdc_positividad', 'flag_vacuna', 'fecha_dosis1', 'fabricante_dosis1',
@@ -14,35 +13,21 @@ from datetime import date
        'prov_domicilio', 'dist_domicilio']"""
 url = "TB_FALLECIDO_HOSP_VAC.csv"
 data = pd.read_csv(url, index_col='id_persona')
-print(data.tail())
 data_na = data.dropna()
-print(data['flag_vacuna'].value_counts())
-print(data.groupby('flag_vacuna')['fecha_fallecimiento'].count())
-print(data['fecha_dosis1'].count() - data['fecha_dosis1'].isna().count())
-print(data.isna().sum())
-print(data['dep_domicilio'].value_counts())
-print(data.columns.array)
 
+print(data.loc[data['dist_domicilio'] == 'LIMA', "edad"])
+data.info(verbose=False)
 # Mapear Rango de Fechas
 
-print(data['fecha_fallecimiento'])
-
-
-# def formatearFechas(fech: str):
-#     fecha = fech.split('/')
-#     map(lambda str: int(str), fecha)
-#     return date.fromisocalendar()
-
-
-fec_fall = data['fecha_fallecimiento']
-# data['fecha_fallecimiento'] = fec_fall.aggregate(formatearFechas)
-print(data['fecha_fallecimiento'])
-
+data["fecha_fallecimiento"] = data["fecha_fallecimiento"].aggregate(pd.to_datetime, axis=0, yearfirst=True, dayfirst=False).head()
+print(data["fecha_fallecimiento"].head())
+range_dates = pd.Series(pd.date_range(start=str(data["fecha_fallecimiento"].min()), end=str(data["fecha_fallecimiento"].max()), periods=6))
+print(data[data["fecha_fallecimiento"] > range_dates[4]])
 # criterios_de_grafica
 options = ["Localidad Departamento"]
-
-with st.sidebar:
-    selected = option_menu("Muertos Por Covid Peru Segun: ", options,
-                           icons=['house', 'gear'], menu_icon="cast", default_index=0)
-if selected == "Localidad Departamento":
-    st.bar_chart(data["dep_domicilio"].value_counts())
+# with st.sidebar:
+#     selected = option_menu("Muertos Por Covid Peru Segun: ", options,
+#                            icons=['house', 'gear'], menu_icon="cast", default_index=0)
+# if selected == "Localidad Departamento":
+#     muertos_by_fecha = data.groupby("fecha_fallecimiento")["sexo"].count()
+#     st.line_chart(muertos_by_fecha)
